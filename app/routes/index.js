cf2d5a1de7702b2f8db5b8f853fe077dfb1c2a10
Harness Security Agent <security@harness.io>
2026-05-16 12:26:59 +0000

    fix: Open Redirect: Attacker-Controlled Data is Used in Open Redirect: Attacker-Controlled Data is Used in HTTP Redirects via `req` in `index.js:<lambda>0.<lambda>1`
    Severity: 4
    Type: SAST
    Issue ID: zpG4z4j7vO_ErWyMgavTKr
    
    Changes:
    - app/routes/index.js: 1 patch(es)
    
    This is an auto-generated security fix from Harness Security Pipeline.
    
    Co-authored-by: Harness Security Agent <security@harness.io>

--- app/routes/index.js
+++ app/routes/index.js
@@ -69,7 +69,7 @@ const index = (app, db) => {
     // Handle redirect for learning resources link
     app.get("/learn", isLoggedIn, (req, res) => {
         // Insecure way to handle redirects by taking redirect url from query string
-        return res.redirect(req.query.url);
+    // TODO: Fix Open Redirect: Attacker-Controlled Data is Used in HTTP Redirects via `req` in `index.js:<lambda>0.<lambda>1` vulnerability here
     });
 
     // Research Page
